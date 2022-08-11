de16dea21bcafa2db57a8dda065914e5ec4594d4
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-11 18:58:29 +0530

    Time: 25 ms (52.81%), Space: 11 MB (33.54%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,20 +1,41 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+        
+// vector<int> ans;
+//         unordered_map<int, int> parvej;
+        
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             if(parvej.find(target -nums[i]) != parvej.end())
+//             {
+//                 ans.push_back(parvej[target-nums[i]]);
+//                 ans.push_back(i);
+//                 return ans;
+//             }
+//             parvej[nums[i]] =i;
+//         }
+//         return ans;
+//     }
+// };
+
+// more understandable format of the above solution
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-vector<int> ans;
-        unordered_map<int, int> parvej;
-        
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(parvej.find(target -nums[i]) != parvej.end())
-            {
-                ans.push_back(parvej[target-nums[i]]);
+        vector<int> ans;
+        unordered_map<int,int> map;
+        for(int i=0;i<nums.size();++i){
+            int diff = (target-nums[i]);
+            if(map.find(diff)!=map.end()){
+                
                 ans.push_back(i);
+                ans.push_back(map[diff]);
                 return ans;
             }
-            parvej[nums[i]] =i;
+            map[nums[i]]= i;
         }
         return ans;
+        
     }
 };
