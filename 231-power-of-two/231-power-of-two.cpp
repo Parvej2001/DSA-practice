80f5031c8d0efbdf51ccde4513cb30b5ac12467e
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-14 22:12:51 +0530

    added comments to the code

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         
-        for(int i=0; i<31; i++){
+        for(int i=0; i<31; i++){       // the loop will run upto 2^31 i.e till 30
             
           int ans= pow(2,i);
             if(ans==n){
