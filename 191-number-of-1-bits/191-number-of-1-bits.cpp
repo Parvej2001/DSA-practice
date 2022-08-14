f6c7f4ab99a8ea953908ad02be1b28794844c39e
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-14 22:10:29 +0530

    added comments to the code

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
+    int hammingWeight(uint32_t n) {   // hammingweight means number of '1' bits
         
         int count = 0;
         
         while(n!=0){
             //checking last digit
-            if(n&1){
+            if(n&1){      
                 count++;
             }
-            n=n>>1;
+            n=n>>1;   // right shift the given bits by 1
         }
         return count;
     }
