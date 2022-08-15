643d55af7fefc960eca6591debbda201fcf9b48c
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-15 23:24:19 +0530

    updated comments

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -5,7 +5,7 @@ public:
         if(x<0 || x>pow(2,31))  // according to the condition value of 'x' should be in this range.
             return false;
         long int num = 0;
-        int y=x;  
+        int y=x;  // intially assigning 'x' to 'y' to compare in the end.
         
         while(x>0){
             num=num*10;
