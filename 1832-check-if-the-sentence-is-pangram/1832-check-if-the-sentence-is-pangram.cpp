77eb35de482f59eb4e2029c51d0410c380524c44
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-17 23:29:25 +0530

    added comments to the code

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool checkIfPangram(string sentence) {
         int n=sentence.size();
+        // here i have used STL 'map' (where map is a container that stores elements in key-value pairs.)
         map<char,int>mp;
         for(int i=0; i<n; i++)
         {
