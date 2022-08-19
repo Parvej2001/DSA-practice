250b4819e6c9bd50f97ad675577296e3044919f9
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-20 01:28:20 +0530

    Time: 4 ms (21.50%), Space: 5.8 MB (95.32%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-        string result="";
-        int n=columnNumber;
-while(n){
-    char c='A'+ (n-1) % 26;
-    result= c+result;
-    n=(n-1)/26;
-}
-return result;
+        
+        // Create an empty string for storing the characters...
+        string output;
+        // Run a while loop while columnNumber is positive...
+        while(columnNumber > 0){
+            // Subtract 1 from columnNumber...
+            columnNumber--;
+            // Get current character by doing modulo of columnNumber by 26...
+            int m = columnNumber % 26;
+            // Divide columnNumber by 26...
+            columnNumber /= 26;
+            output += 'A' + m;
+        }
+        // Reverse the output string...
+        // Bcause we have found characters from right to left...
+        reverse(output.begin(),output.end());
+        // Return the reversed string.
+        return output;
+        
+     
         
         // another solution 
+//          string result="";
+//         int n=columnNumber;
+// while(n){
+//     char c='A'+ (n-1) % 26;
+//     result= c+result;
+//     n=(n-1)/26;
+// }
+// return result;
         
     }
 };
