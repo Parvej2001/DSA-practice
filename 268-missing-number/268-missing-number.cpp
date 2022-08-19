5b2bd9778475a4d95edbf3655b44d7505c11c3a2
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-20 00:18:54 +0530

    added comments to the code

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -5,10 +5,15 @@ public:
         int sum_given_nums = 0;
         
         for (int i = 0; i < nums.size(); i++){
-            sum_n_nums += i+1;
+            sum_n_nums += i+1;    // We do sum_n_nums += i+1; to get the sum of [1, n+1] and not [0, n]
             sum_given_nums += nums[i];
         }
         
         return sum_n_nums - sum_given_nums;
     }
 };
+
+// The idea is we first sum all integers [1, n+1], 
+//then we sum all integers we have in the array and the subtract them. This will give us the missing integer.
+//Time Complexity: O(n)
+//Space Complexity: O(1)
