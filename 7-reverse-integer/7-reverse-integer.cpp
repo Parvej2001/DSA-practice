047c45b0bd132fcf16bf87f3d9ce5b5b9c957f9a
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-20 12:36:31 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.64%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int reverse(int x) {
         
-        int ans=0;
+        int ans=0;   
         while(x!=0){
             
-            int digit=x%10;
+            int digit=x%10;    // taking last number 
             
-            if((ans>INT_MAX/10) || ans<INT_MIN/10 ){
+            if((ans>INT_MAX/10) || ans<INT_MIN/10 ){  // checking the range
                 return 0;
             }
             
-            ans=(ans*10) + digit;
-            x=x/10;
+            ans=(ans*10) + digit;   // storing the number
+            x=x/10;     // taking qoutient i.e updating the number
         }
     return ans;
     }
