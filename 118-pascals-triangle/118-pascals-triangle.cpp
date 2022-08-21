b728177271838e6f080e529acb9dfa83640f10f1
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-21 19:14:06 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.76%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-      vector<vector<int>> r(numRows);  
+      vector<vector<int>> r(numRows);  // creating a dynamic vector and keeping it empty
         
-        for(int i=0; i<numRows; i++)
+        for(int i=0; i<numRows; i++) // traversing through the 'n' rows 
         {
-            r[i].resize(i+1);
-            r[i][0]=r[i][i]=1;
+            r[i].resize(i+1);  // since the row was empty , resizeing it
+            r[i][0]=r[i][i]=1;  // as first and last column in the triangle is 1 , hence initializing with 1
             
-            for(int j=1; j<i; j++)
-                r[i][j]=r[i-1][j-1]+r[i-1][j];
+            for(int j=1; j<i; j++) // now linearly traversing from first column to the last column
+                r[i][j]=r[i-1][j-1]+r[i-1][j];     // adding the value of the previous rows
         }
         return r;
     }
