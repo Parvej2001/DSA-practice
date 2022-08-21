7d8a7e6eeda52043d3810792a8365a9b6fd17cf4
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-21 21:26:07 +0530

    Time: 160 ms (49.71%), Space: 52.3 MB (15.04%) - LeetHub

---
+++ 888-fair-candy-swap/888-fair-candy-swap.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+          int m=aliceSizes.size(); 
+        int n=bobSizes.size();
+      // finding there sum
+        
+        int a=0,b=0;   // alice and bob
+        
+        for(int i=0;i<m;i++){  
+            a+=aliceSizes[i];  // alice sum
+        }
+        for(int j=0;j<n;j++){
+            b+=bobSizes[j];   // bob sum
+        }
+        int diff=(b-a)/2;   
+        
+        set<int>s; //each element has to be unique because the value of the element identifies it.
+        for(auto i:aliceSizes){
+            s.insert(i);
+        }
+        vector<int>ans;
+        for(auto j:bobSizes){
+            if(s.find(j-diff)!=s.end()){
+                ans.push_back(j-diff);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
