1f5275404cf28e986e378196f4cca1742f186074
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-23 10:43:05 +0530

    Time: 13 ms (14.18%), Space: 12.4 MB (29.75%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -2,19 +2,41 @@ class Solution {
 public:
     int findMiddleIndex(vector<int>& nums) {
     
-        int n=nums.size();
-        int currentsum=0;
-        int totalsum=0;
-        for(int i=0; i<n; i++)
+        // int n=nums.size();
+        // int currentsum=0;
+        // int totalsum=0;
+        // for(int i=0; i<n; i++)
+        // {
+        //     totalsum+=nums[i];
+        // }
+        // for(int i=0; i<n; i++)
+        // {
+        //     if(currentsum==totalsum-currentsum-nums[i]){
+        //         return i;
+        //     }
+        //     currentsum+=nums[i];
+        // }
+        // return -1;
+        
+        // more optamize solution.
+        
+        int sum = 0 ; 
+        for(auto i : nums)
+            sum+=i;
+        
+        int leftsum = 0 ;
+        int rightsum = sum-nums[0] ;
+        
+        // For 0 Index
+         if(leftsum == rightsum)
+                return 0;
+        
+        for(int i = 1 ; i <nums.size() ; i++)
         {
-            totalsum+=nums[i];
-        }
-        for(int i=0; i<n; i++)
-        {
-            if(currentsum==totalsum-currentsum-nums[i]){
+            leftsum+= nums[i-1];
+            rightsum -= nums[i];
+            if(leftsum == rightsum)
                 return i;
-            }
-            currentsum+=nums[i];
         }
         return -1;
     }
