cb91a90401d3f623840fe2313b5e8e2133be0e30
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-25 23:01:31 +0530

    updated commnets

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -10,7 +10,7 @@ public:
         //     }
         // }
         
-        // two poimter approach
+        // two pointer approach
         int j=0;
         for(int i=0; i<nums.size(); i++){
             if(nums[i]!=0)
