aab924e52ef5215840dca23436f6562da42e2c29
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-26 23:21:36 +0530

    added comments to the code

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
        
-         if(nums.empty())return {};
+        if(nums.empty())return {}; // return nothing if it is null
         vector<int>ans;
-        sort(begin(nums),end(nums));
-        int s = nums[0];
+        sort(begin(nums),end(nums));   // sorting the array 
+        int s = nums[0];    // initially storing the value of null
         for(int i=1;i<nums.size();i++){
             if(!(s^nums[i])){
                 ans.push_back(nums[i]),i+=1;
