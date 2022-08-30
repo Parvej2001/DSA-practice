1148210cbf666a7a7fc006afa79f39175d8d955e
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-08-30 23:05:45 +0530

    added comments

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -4,16 +4,16 @@ public:
         // int n=nums.size();
         // int product=1;
         // for(int i=0; i<n; i++){
-        //      product=product*nums[i];
+        //      product=product*nums[i];           
         // }
         // return product;
         
         int n=nums.size();
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());              //Time complexity   O(nlogn)
         int x=nums[n-1]*nums[n-2]*nums[n-3];
         int y=nums[0]*nums[1]*nums[n-1];
         return max(x,y);
     }
 };
 
-//Time complexity   O(nlogn)
+
