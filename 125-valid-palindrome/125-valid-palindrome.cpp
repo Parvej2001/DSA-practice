c24be4561c458de69bc5db26f1667d1a9c968150
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-09-02 17:41:22 +0530

    Time: 17 ms (9.99%), Space: 7.6 MB (40.30%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s)
+{
+    string str = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (isalnum(s[i])) // check if a character is alphanumeric or not
+            str.push_back(tolower(s[i]));
+    }
+    for (int i = 0; i < str.size() / 2; i++)
+    {
+        if (str[i] != str[str.size() - 1 - i]) // if str is a palindrome then this condition would always be false
+            return false;
+    }
+    return true;
+}
+};
