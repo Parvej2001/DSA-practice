803ca0e923b03b2259d64878f5dee6471c61c898
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-09-03 23:25:49 +0530

    Time: 74 ms (5.13%), Space: 5.9 MB (31.14%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         
-        for(int i=0; i<31; i++){
-            double ans = pow(3,i);
-            if(ans==n){
-                return true;
-            }
-        }
-        return false;
-    }
+    //     for(int i=0; i<31; i++){
+    //         double ans = pow(3,i);
+    //         if(ans==n){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+        if (n<=0) return false;
+        if (n == 1) return true;
+        return (n%3==0 && isPowerOfThree(n/3));
+}
 };
