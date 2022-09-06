2fef76c4184ad46970e2f9ded7b7486ff78d4ff1
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-09-06 21:51:08 +0530

    Time: 0 ms (100.00%), Space: 6 MB (46.09%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -8,12 +8,31 @@ public:
         return ans; */
         
     // Using recursion     
-         if(n<0) return 1/x*myPow(1/x, -(n+1));
+         /*if(n<0) return 1/x*myPow(1/x, -(n+1));
         
         if(n==0) return 1;
         if(n==1) return x;
         if(n==2) return x*x;
         if((n&1)==0) return myPow(myPow(x, n/2), 2);
-        else return x*myPow(myPow(x, n/2), 2);
+        else return x*myPow(myPow(x, n/2), 2); */
+        
+    // using striver approach
+         if(n==0){return 1;}
+    else if(n==1){return x;}
+    else if(n>0){
+        if(n%2==0){
+            x=x*x;
+            n= n/2;   
+        }
+            return x*myPow(x,n-1);
+    }
+    else {
+        if(n%2==0){
+            x=x*x;
+            n= n/2;   
+        }
+            return 1/x*myPow(x,n+1);
+    }
+
     }
 };
