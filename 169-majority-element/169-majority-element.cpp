b904079bd5c158af806d79f547d685bebd969843
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-09-07 23:10:36 +0530

    Time: 39 ms (27.50%), Space: 19.4 MB (92.28%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,35 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
-//         int n=nums.size();
-        
-//         unordered_map<int,int> m;  // since we can kmow the number of occurance of elemnets
-        
-//         for(auto a: nums)    // using ranged based for loop
-//         {
-//             m[a]++;   
-//             if(m[a]> n/2) return a;
-//         }
-//         return -1;
-        
-        
-        // more optimal solution : TC= O(n),   SC= O(n) USING more voting algorithm
-        
-        int n=nums.size();
-        int candidate=0;
-        int vote=0;
-        
-        for(auto a: nums)   
-        {
-           if(vote==0){
-               candidate=a;
-           } 
-            if(candidate==a)
-                vote++;
-            else
-                vote--;     
-        }
-         return candidate;
+        sort(nums.begin(), nums.end());
+        return nums[(int)nums.size()/2];
     }
 };
