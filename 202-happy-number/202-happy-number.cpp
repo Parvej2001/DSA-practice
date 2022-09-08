1f74c1da9635613af0865cd1eeca65f617a790b3
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-09-08 23:32:36 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (84.74%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    int calculate(int n){
+        int sum = 0;
+        while(n){
+            int dig = n % 10;
+            sum += (dig * dig);
+            n /= 10;
+        }
+        return sum;
+    }
+    
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = calculate(n);
+        
+        while(fast != 1 && slow != fast){
+            slow = calculate(slow);
+            fast = calculate(calculate(fast));
+        }
+        
+        return fast == 1;
+    }
+};
