2a708c92465476cf832ca19e1ab1e132433663cf
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-09-10 17:20:22 +0530

    Time: 9 ms (13.48%), Space: 9.2 MB (30.28%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+         vector<int> ans;  //new vector to store the merged sorted array
+        int i = 0;
+        int j = 0;
+		/* Pushing the elements in the ans vector by comparing 
+		the elements  of nums1 and nums2 */
+        while(i<m && j<n){
+            if(nums1[i] <= nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                ans.push_back(nums2[j]);
+                j++;
+            }
+        }
+		// pushing the elements in case some elements are left in nums1
+        while(i < m){
+            ans.push_back(nums1[i]);
+            i++;
+        }
+		// pushing the elements in case some elements are left in nums2
+         while(j < n){
+            ans.push_back(nums2[j]);
+            j++;
+        }
+
+        nums1.clear(); // clearing the nums1 to store the merged array
+
+        for(int i = 0; i<m+n; i++){
+            nums1.push_back(ans[i]);  // copying ans into nums1
+        }
+    }
+};
