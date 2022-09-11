be62698b38b4ef295ee83ae3e8a600a3d6fb389a
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-09-11 17:40:02 +0530

    added codes

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,18 +1,35 @@
 // using 2 pointer approach 
 
-
-
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int original_size = nums.size();
-        for(int i = 0; i < original_size; i++){
-            nums[i] = (nums[i])*(nums[i]);
+        int i=0,j=nums.size()-1;
+        int k=nums.size()-1;
+        vector<int>ans(nums.size());
+        while(i<=j){
+            if(abs(nums[i])>abs(nums[j])){
+                ans[k--]=nums[i]*nums[i];
+                i++;
+            }else{
+                ans[k--]=nums[j]*nums[j];
+                j--;
+            }
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         int original_size = nums.size();
+//         for(int i = 0; i < original_size; i++){
+//             nums[i] = (nums[i])*(nums[i]);
+//         }
+//         sort(nums.begin(), nums.end());
+//         return nums;
+//     }
+// };
 
 // My solution 
 // class Solution {
