57be4ad739bde085592afa81f42bd19a50964922
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-09-21 22:45:15 +0530

    Update 509-fibonacci-number.cpp

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int fib(int n) { 
-           // this problem is based on recursion but still need to study . initially i wrote program for fibonacci series but then i realise
-        // its something different.
+         
  //         int a=0, b=1, c;  
 //         for(int i=0; i<n; i++)
 //         {
