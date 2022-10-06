615d4832d570b392fda390ac2d7c8ca47fa7fa59
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-10-06 23:07:36 +0530

    Time: 7 ms (21.10%), Space: 8.5 MB (98.35%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,16 +1,32 @@
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         size_t i = digits.size();
+//         while (++digits[--i] == 10)
+//         {
+//             digits[i] = 0;
+//             if (!i)
+//             {
+//                 digits.insert(digits.begin(), 1);
+//                 break;
+//             }            
+//         }
+//         return digits;
+//     }
+// };
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        size_t i = digits.size();
-        while (++digits[--i] == 10)
-        {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
             digits[i] = 0;
-            if (!i)
-            {
-                digits.insert(digits.begin(), 1);
-                break;
-            }            
         }
+        
+        digits[0] = 1;
+        digits.push_back(0);
         return digits;
     }
 };
