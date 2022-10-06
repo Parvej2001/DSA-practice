0e53b7558932e369fa1b150f58394903b637f2d4
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2022-10-06 23:29:42 +0530

    Time: 4 ms (44.52%), Space: 8.8 MB (12.46%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -14,19 +14,55 @@
 //         return digits;
 //     }
 // };
+
+// neet code solution
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         for (int i = digits.size() - 1; i >= 0; i--) {
+//             if (digits[i] < 9) {
+//                 digits[i]++;
+//                 return digits;
+//             }
+//             digits[i] = 0;
+//         }
+        
+//         digits[0] = 1;
+//         digits.push_back(0);
+//         return digits;
+//     }
+// };
+
+// Fraz solution
+
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& digits) {
-        for (int i = digits.size() - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
-        }
+    vector<int> plusOne(vector<int>& d) {
         
-        digits[0] = 1;
-        digits.push_back(0);
-        return digits;
+        for(int i=d.size()-1; i>=0; i--)
+        {
+            if(d[i]<9){
+                d[i]++;
+                return d;
+            }else{
+                d[i]=0;
+            }
+        }
+        d.push_back(0);
+        d[0]=1;
+        return d;
+        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
