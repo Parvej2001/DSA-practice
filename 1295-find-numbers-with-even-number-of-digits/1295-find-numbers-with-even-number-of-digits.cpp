905b7eae6220b16a2300a3c02a29fd44145a696f
PARVEJ SHAIKH <86014533+Parvej2001@users.noreply.github.com>
2023-07-10 23:51:56 +0530

    Update 1295-find-numbers-with-even-number-of-digits.cpp

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-	int findNumbers(vector<int>& nums) {  // creating arrays
+	int findNumbers(vector<int>& nums) {  // creating arrays of the given values
 		int ans=0;
 		for(int i=0;i<nums.size();i++)
 		{
